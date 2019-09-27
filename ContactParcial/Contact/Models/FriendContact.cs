8b36e85133af4e823b2a79059a8146ac65599234
alexxsala@gmail.com <alexxsala@gmail.com>
2019-09-27 19:47:37 -0400

    primer commit

---
+++ ContactParcial/Contact/Models/FriendContact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Contact.Models
+{
+    public class FriendContact
+    {
+        public enum ListType
+        {
+            Alejandra,
+            Rodrigo,
+            Rossy,
+            Carmelo,
+            Bernarda
+        }
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        [Range(5,50)]
+        public string name { get; set; }
+
+        public ListType LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+
+        public int MyProperty { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+    }
+}
