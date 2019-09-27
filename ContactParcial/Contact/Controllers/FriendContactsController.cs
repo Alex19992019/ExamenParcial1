918f6c171b7d4df4b2310b36d69fc22a0401c82e
alexxsala@gmail.com <alexxsala@gmail.com>
2019-09-27 19:59:31 -0400

    tercer commit

---
+++ ContactParcial/Contact/Controllers/FriendContactsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Contact.Models;
+
+namespace Contact.Controllers
+{
+    public class FriendContactsController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/FriendContacts
+        public IQueryable<FriendContact> GetFriendContacts()
+        {
+            return db.FriendContacts;
+        }
+
+        // GET: api/FriendContacts/5
+        [ResponseType(typeof(FriendContact))]
+        public IHttpActionResult GetFriendContact(int id)
+        {
+            FriendContact friendContact = db.FriendContacts.Find(id);
+            if (friendContact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(friendContact);
+        }
+
+        // PUT: api/FriendContacts/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFriendContact(int id, FriendContact friendContact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != friendContact.FriendId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(friendContact).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FriendContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/FriendContacts
+        [ResponseType(typeof(FriendContact))]
+        public IHttpActionResult PostFriendContact(FriendContact friendContact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.FriendContacts.Add(friendContact);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = friendContact.FriendId }, friendContact);
+        }
+
+        // DELETE: api/FriendContacts/5
+        [ResponseType(typeof(FriendContact))]
+        public IHttpActionResult DeleteFriendContact(int id)
+        {
+            FriendContact friendContact = db.FriendContacts.Find(id);
+            if (friendContact == null)
+            {
+                return NotFound();
+            }
+
+            db.FriendContacts.Remove(friendContact);
+            db.SaveChanges();
+
+            return Ok(friendContact);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FriendContactExists(int id)
+        {
+            return db.FriendContacts.Count(e => e.FriendId == id) > 0;
+        }
+    }
+}
---
+++ ContactParcial/Contact/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Contact.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Contact.Models.FriendContact> FriendContacts { get; set; }
+    }
+}
